d5f29d0a63cf7d67250c0ea9b705843dd2940754
Denyyys <94078181+denyyys@users.noreply.github.com>
2023-03-05 21:56:34 +0100

    added ping_hosts ps script

---
+++ powershell/ping_hosts.ps1
@@ -0,0 +1,10 @@
+﻿$HostsIP = "192.168.0.1", "192.168.0.199", "192.168.1.254"
+
+foreach ($host_ip in $HostsIP) {
+    if (Test-Connection -ComputerName $host_ip -Count 1 -Quiet) {
+        Write-Host "$host_ip `t`t [OK]" -ForegroundColor Green
+    }
+    else {
+        Write-Host "$host_ip `t`t [FAIL]" -ForegroundColor Red
+    }
+}
