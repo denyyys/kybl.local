94c7c45fc370b09a329cae579d2f10e066234af4
Denyyys <94078181+denyyys@users.noreply.github.com>
2024-02-23 14:38:44 +0100

    Add files via upload

---
+++ powershell/convert_rsc.ps1
@@ -0,0 +1,12 @@
+﻿$ipAddressesFilePath = "X:\data.txt"
+
+$addressListName = "tor-blacklist"
+
+$ipAddresses = Get-Content -Path $ipAddressesFilePath
+
+$scriptContent = "/ip firewall address-list`n"
+$scriptContent += foreach ($ip in $ipAddresses) {
+    "add list=$addressListName address=$ip`n"
+}
+
+$scriptContent | Out-File -FilePath "D:\tmp\blacklist_script.rsc" -Encoding UTF8
